6096f683e601b7c1dbc43fcc3a71838c952e6aeb
jacksopy <148472316+jacksopy@users.noreply.github.com>
2025-04-09 23:54:58 +0200

    Update req.sh

--- req.sh
+++ req.sh
@@ -1,6 +1,7 @@
 chmod +x 2.sh
 chmod +x osint.sh
 chmod +x jokair_scripting.sh 
+chmod +x update.sh
 chmod +x lookup.sh
 mkdir other_pkg
 cd other_pkg 
@@ -13,7 +14,9 @@ cd ..
 cd instakit 
 pip install -r requirements.txt
 cd ..
+git clone https://github.com/s0md3v/XSStrike.git
+cd XSStrike
+pip install -r requirements.txt
 cd ..
-mkdir database
-sudo apt install  jq
-
+cd ..
+sudo apt install jq
