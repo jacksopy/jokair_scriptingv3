4176af8685c5ab1106ec0368a521225f2d5619df
jacksopy <148472316+jacksopy@users.noreply.github.com>
2025-04-09 23:54:33 +0200

    Update osint.sh

--- osint.sh
+++ osint.sh
@@ -5,12 +5,6 @@ echo"⠀⠀⠀⠀⣀⠤⠔⠒⠶⠏⠀⠀⠀⠀⠀⠀⠀⠹⡀⠀⠀⢀⡎⠀⠀
 echo"⠀⠀⡴⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣇⠀⠀⣸⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢦⡀"⠀
 echo"⠀⡼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠔⠚⠉⠉⠀⠀⠉⠉⠓⠢⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢳"⠀
 echo"⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⣀⣠⡤⣤⡀⠀⠀⠀⣤⡤⣄⣀⠙⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸"⠀
-echo"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-echo"⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠊⠉⠉⠉⠓⢦⡀⠀⠀⠀⠀⢀⡤⠚⠉⠉⠉⠑⠦⡀"⠀⠀⠀⠀⠀⠀⠀⠀⠀
-echo"⠀⠀⠀⠀⣀⠤⠔⠒⠶⠏⠀⠀⠀⠀⠀⠀⠀⠹⡀⠀⠀⢀⡎⠀⠀⠀⠀⠀⠀⠀⠙⠶⠒⠲⠤⣄"⠀⠀⠀⠀
-echo"⠀⠀⡴⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣇⠀⠀⣸⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢦⡀"⠀
-echo"⠀⡼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠔⠚⠉⠉⠀⠀⠉⠉⠓⠢⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢳"⠀
-echo"⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⣀⣠⡤⣤⡀⠀⠀⠀⣤⡤⣄⣀⠙⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸"⠀
 echo"⠀⢧⠀⠀⠀⠀⠀⠀⠀⠀⢀⡞⠀⠸⠛⠉⠁⠉⠀⠀⠀⠀⠉⠉⠉⠛⢥⠀⢳⡀⠀⠀⠀⠀⠀⠀⠀⢀⣼"⠀
 echo"⠀⠘⣧⠀⠀⠀⠀⠀⠀⠀⡾⠀⡴⠋⠉⠉⠳⣄⠀⠀⠀⠀⢠⠖⠉⠉⠙⢦⠀⢧⠀⠀⠀⠀⠀⠀⠀⣼"⠀
 echo"⢀⡞⠁⠀⠀⠀⠀⠀⠀⣾⠃⡜⠀⠀⠀⡆⠀⠘⡆⠀⠀⢰⠃⠀⢰⠀⠀⠀⢳⠈⡇⠀⠀⠀⠀⠀⠀⠀⢷⡀"
@@ -31,14 +25,14 @@ echo"⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⠿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠉
 echo"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉"⠀⠀⠀⠀⠀⠀⠀⠀⠀
 echo"⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⣀⣀⣀⡀"⠀⠀⠀⠀⠀⠀⠀
 echo"⠀⠀⠀⠀⠀⠀⠛⠛⠓⠛⠙⠋⠋⠛⠟⠛⠟⠃⠚⠻⠛⠛⠀⠀⠃⠸⠛⠛⠓⠏⠟⠛⠰⠟⠂"⠀⠀⠀⠀⠀
-echo "+------------------------------------------------------------------------"
-echo "/                                  osint                                /"
-echo "+-----------+-----------+--------------+--------------+-----------------+"
-echo "/ sherlock  / ip lookup / phone lookup /   instakit   / lookup local db /"
-echo "+-----------+-----------+--------------+--------------+-----------------+"
-echo "/     1     /     2     /       3      /       4      /        5        /"
-echo "+-----------+-----------+--------------+--------------+-----------------+"
-echo "choose a tool      TOOLS1 97          TOOLS2 98          99 leave"
+echo "+--------------------------------------------------------------------------------------------------+"
+echo "/                                                osint                                             /"
+echo "+-----------+-----------+--------------+--------------+-----------------+----------+---------------+"
+echo "/ sherlock  / ip lookup / phone lookup /   instakit   / lookup local db /   whois  /   enum4linux  /"   
+echo "+-----------+-----------+--------------+--------------+-----------------+----------+---------------+"
+echo "/     1     /     2     /       3      /       4      /        5        /     6    /        7      /"   
+echo "+-----------+-----------+--------------+--------------+-----------------+----------+---------------+"
+echo "choose a tool      TOOLS1 97    connection 101      TOOLS2 98          99 leave"
 read tool_choice
 if [ $tool_choice = 1 ]; then
     echo "met le username de la personne"
@@ -47,7 +41,7 @@ if [ $tool_choice = 1 ]; then
 elif [ $tool_choice = 2 ]; then
     ip-tracer start
 elif [ $tool_choice = 3 ]; then
-    echo "ta mis l'api ou pas?"
+    echo "ta mis l'api ou pas?" 
     echo "y/n"
     read reel
     if [ $reel = n ]; then
@@ -55,8 +49,8 @@ elif [ $tool_choice = 3 ]; then
     elif [ $reel = y ]; then
         echo "met le numero de la personne"
         read numero
-        ./lookup.sh
-    else
+        ./lookup.sh $numero
+    else 
         clear
         ./osint.sh
     fi
@@ -64,18 +58,28 @@ elif [ $tool_choice = 4 ]; then
     cd other_pkg
     cd instakit
     python3 main.py
-elif [ $tool_choice = 4 ]; then
+elif [ $tool_choice = 5 ]; then
     cd database
     echo "entrer le string a lookup"
     read string
     cat * | grep $string
+elif [ $tool_choice = 6 ]; then
+    echo "mettez le dns a enumerer"
+    read dns
+    whois $dns
+elif [ $tool_choice = 7 ]; then
+    echo "mettez le dns ou l'ip a enumerer"
+    read dns
+    enum4linux $dns 
 elif [ $tool_choice2 = 97 ]; then
     clear
     ./jokair_scripting.sh
 elif [ $tool_choice2 = 98 ]; then
     clear
     ./2.sh
-else
+elif [ $tool_choice2 = 99 ]; then
+    echo "goodbye"
+else 
     clear
     ./osint.sh
 fi
